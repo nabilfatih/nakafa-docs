cbd1c4f1eae6c7982b23dac78b8594f2d549b2c0
nabilfatih <nabilakbarazzima@gmail.com>
2023-01-11 19:36:22 +0100

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Logo from "./components/Logo";
 
 const config: DocsThemeConfig = {
-  logo: <span>Nakafa</span>,
+  logo: (
+    <>
+      <Logo />
+      <strong>Nakafa</strong>
+    </>
+  ),
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    return (
+      <>
+        <meta property="og:url" content={`https://docs.nakafa.com${asPath}`} />
+        <meta property="og:title" content={frontMatter.title || "Nakafa"} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || "Nkafa Documentation"}
+        />
+      </>
+    );
+  },
   project: {
     link: "https://github.com/nabilfatih/nakafa-docs",
   },

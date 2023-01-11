76d130d0a4f08f776d50e3e5cab291f5cd477652
nabilfatih <nabilakbarazzima@gmail.com>
2023-01-11 19:10:22 +0100

    updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Nakafa</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/nabilfatih/nakafa-docs",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/nabilfatih/nakafa-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nakafa Documentation",
   },
-}
+  i18n: [],
+};
 
-export default config
+export default config;
